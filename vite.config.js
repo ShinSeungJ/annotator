7477414feec5277dca2c4cef90c1b32a321a855e
SSJ <shin93754@gmail.com>
2025-07-25 16:31:13 +0900

    fix: updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/annotator/',
   server: {
     port: 5174,
     host: true, // allows access from LAN
